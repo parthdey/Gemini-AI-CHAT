1dae8c04c40aab1e33dc932ba9fa33bced397825
Parth_dey <130208653+parthdey@users.noreply.github.com>
2025-08-16 00:53:58 +0530

    Initial commit - added gemini-chat and gemini-chat-frontend

---
+++ gemini-chat/src/main/java/com/ai/gemini_chat/QnAService.java
@@ -0,0 +1,54 @@
+package com.ai.gemini_chat;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+public class QnAService {
+    // Access to APIKey and URL [Gemini]
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+//    @PostConstruct
+//    public void debugVars() {
+//        System.out.println("URL: " + geminiApiUrl);
+//        System.out.println("KEY: " + geminiApiKey);
+//    }
+
+    private final WebClient webClient;
+
+    public QnAService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
+    public String getAnswer(String question) {
+        // Construct the request payload
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[] {
+                                Map.of("text", question)
+                        } )
+                }
+        );
+
+        // Make API Call
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        // Return response
+        return response;
+
+    }
+}
